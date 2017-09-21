96829ee47e5aedaa4031aeda971131ef13c74ff2
Jennifer Shopsin <jennifer.shopsin@gmail.com>
2017-09-21 14:35:22 -0700

    Add selection of cards

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,4 +6,22 @@
 
 $(document).ready(function() {
   console.log("ready!");
+
+  $(".board").on("click", ".card-in-play", function(event){
+    event.preventDefault();
+    console.log("AYYYYYYYY");
+
+    board = $(this).closest(".board");
+
+    $(this).find("img").toggleClass("active");
+
+    console.log(board);
+    console.log(board.find(".active").length);
+
+    if(board.find(".active").length === 3){
+      console.log("YO have selected 3")
+    }
+
+
+  })
 });
