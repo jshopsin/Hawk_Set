a72d17591e1115ed71fd7b41cbbcaf6d3b32cbca
Jennifer Shopsin <jennifer.shopsin@gmail.com>
2017-09-21 12:17:05 -0700

    Fix bug for new game button

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -6,25 +6,26 @@ class GamesController < ApplicationController
 
   def create
     game = Game.new(user_id: current_user.id)
-    cards = Card.all
-    board = cards.sample(12)
-    deck = []
-
-    cards.each do |card|
-      if !board.include?(card)
-        deck << card
-      end
-    end
-
-    board.each do |card|
-      game.assignments.create!(card_id: card.id, card_status: "in_play")
-    end
-
-    deck.each do |card|
-      game.assignments.create!(card_id: card.id, card_status: "remaining")
-    end
 
     if game.save
+      cards = Card.all
+      board = cards.sample(12)
+      deck = []
+
+      cards.each do |card|
+        if !board.include?(card)
+          deck << card
+        end
+      end
+
+      board.each do |card|
+        game.assignments.create!(card_id: card.id, card_status: "in_play")
+      end
+
+      deck.each do |card|
+        game.assignments.create!(card_id: card.id, card_status: "remaining")
+      end
+
       redirect_to game_path(game)
     else
       redirect_to root_path
