8b2302b871a922f58188149c5ebec73dd7e81999
Jennifer Shopsin <jennifer.shopsin@gmail.com>
2017-09-21 18:26:13 -0700

    Working board when selecting 3. Does not update the board unless a refresh is done.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,27 +1,5 @@
-//= require rails-ujs
-//= require_tree .
-
 //= require jquery
 //= require jquery_ujs
 
-$(document).ready(function() {
-  console.log("ready!");
-
-  $(".board").on("click", ".card-in-play", function(event){
-    event.preventDefault();
-    console.log("AYYYYYYYY");
-
-    board = $(this).closest(".board");
-
-    $(this).find("img").toggleClass("active");
-
-    console.log(board);
-    console.log(board.find(".active").length);
-
-    if(board.find(".active").length === 3){
-      console.log("YO have selected 3")
-    }
-
-
-  })
-});
+//= require rails-ujs
+//= require_tree .
--- app/controllers/card_sets_controller.rb
+++ app/controllers/card_sets_controller.rb
@@ -1,15 +1,40 @@
 class CardSetsController < ApplicationController
   def create
-    @new_card_set = CardSet.create()
+    game = Game.find(params[:game_id])
 
-    @game = Game.find(params[:game_id])
+    respond_to do |format|
+      format.html { redirect_to game_path(game) }
+      format.json {
+        card1 = Card.find_by(image_url: (params["cards"]["c1"].downcase.split(" ").join("_") + ".png"))
+        card2 = Card.find_by(image_url: (params["cards"]["c2"].downcase.split(" ").join("_") + ".png"))
+        card3 = Card.find_by(image_url: (params["cards"]["c3"].downcase.split(" ").join("_") + ".png"))
 
-    @cards = [ Card.find(params[:card1]), Card.find(:card2), Card.find(params[:card3]) ]
+        cards = [card1, card2, card3]
 
+        # if is_set?(cards)
+        if cards.length == 3
+          new_card_set = CardSet.create!()
 
-    @cards.each do |card|
-      Assignment.find_by_card_id_and_game_id(card.id, @game.id).update(card_status: "in_set", card_set_id: @new_card_set.id)
+          cards.each do |card|
+            Assignment.find_by_card_id_and_game_id(card.id, game.id).update(card_status: "in_set", card_set_id: new_card_set.id)
+          end
+
+          deck = game.assignments.remaining
+
+          new_cards = deck.sample(3)
+
+          new_cards.each do |assignment|
+            assignment.update(card_status: "in_play")
+          end
+
+          game_path(game.id)
+          return "ppp"
+        else
+          redirect_to game_path(game.id)
+          flash.now[:alert] = 'Not a valid set'
+        end
+      }
     end
-
   end
+
 end
